4808b386094376acac92b05ed7a6b37e42d66bc5
Le Thai Phuc Quang <phuc.quang102@gmail.com>
2015-04-29 00:41:26 +0700

    Initial Commit

---
+++ MSPYEPSeedingClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MSPYEPSeedingClient
+//
+//  Created by Le Thai Phuc Quang on 4/29/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MSPYEPSeedingClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSPYEPSeedingClient
+//
+//  Created by Le Thai Phuc Quang on 4/29/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
